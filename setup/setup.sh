2d96c43640cca98ab2919755647efa60223be183
Chia Wei <43131747+weichia2017@users.noreply.github.com>
2021-01-13 21:17:49 +0800

    updated setup for conda enviroment

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,1 +1,2 @@
-while read requirement; do conda install --yes $requirement; done < requirements.txt
+while read requirement; do conda install --yes $requirement; done < conda.txt
+while read requirement; do pip install --yes $requirement; done < pip.txt 
