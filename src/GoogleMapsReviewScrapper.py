8f6fb01899b33d2e4172981eba0d3b4e27fcd325
Chia Wei <43131747+weichia2017@users.noreply.github.com>
2021-01-13 21:19:31 +0800

    Create GoogleMapsReviewScrapper
    Create GoogleMapReviewScrapper Object to perform scrapping.

---
+++ src/GoogleMapsReviewScrapper.py
@@ -0,0 +1,151 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+from datetime import datetime
+import time, re, logging, traceback
+
+MAX_WAIT = 10
+MAX_RETRY = 5
+DEFAULT_PULL = 100
+
+class Scraper:
+    def __init__(self, debug=False):
+        self.debug = debug
+        self.driver = self.__get_driver()
+        self.logger = self.__get_logger()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, tb):
+        if exc_type is not None:
+            traceback.print_exception(exc_type, exc_value, tb)
+        self.driver.close()
+        self.driver.quit()
+        return True
+
+    def setURL(self, name, url):
+        self.logger.warn('Getting ' + name + ' at ' + url)
+        self.driver.get(url)
+        self.logger.warn('Loaded ' + name)
+
+    def sort_by_date(self):
+        wait = WebDriverWait(self.driver, MAX_WAIT)
+        clicked = False
+        tries = 0
+        while not clicked and tries < MAX_RETRY:
+            try:
+                menu_bt = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@data-value=\'Sort\']')))
+                menu_bt.click()
+                clicked = True
+                time.sleep(3)
+            except Exception as e:
+                tries += 1
+                self.logger.warn('Failed to click recent button, attempt: '+ str(tries))
+            if tries == MAX_RETRY:
+                return -1
+        recent_rating_bt = self.driver.find_elements_by_xpath('//li[@role=\'menuitemradio\']')[1]
+        recent_rating_bt.click()
+        time.sleep(5)
+        return 0
+
+    def get_reviews(self, offset):
+        self.__scroll()
+        time.sleep(4) 
+        self.__expand_reviews()
+        response = BeautifulSoup(self.driver.page_source, 'html.parser')
+        rblock = response.find_all('div', class_='section-review-content')
+        parsed_reviews = []
+        for index, review in enumerate(rblock):
+            if index >= offset:
+                parsed_reviews.append(self.__parse(review))
+        return parsed_reviews
+
+    def __parse(self, review):
+        item = {}
+        id_review = review.find('button', class_='section-review-action-menu')['data-review-id']
+        username = review.find('div', class_='section-review-title').find('span').text
+        try:
+            review_text = self.__filter_string(review.find('span', class_='section-review-text').text)
+        except Exception as e:
+            review_text = None
+        rating = float(review.find('span', class_='section-review-stars')['aria-label'].split(' ')[1])
+        relative_date = review.find('span', class_='section-review-publish-date').text
+
+        try:
+            n_reviews_photos = review.find('div', class_='section-review-subtitle').find_all('span')[1].text
+            metadata = n_reviews_photos.split('\xe3\x83\xbb')
+            if len(metadata) == 3:
+                n_photos = int(metadata[2].split(' ')[0].replace('.', ''))
+            else:
+                n_photos = 0
+            idx = len(metadata)
+            n_reviews = int(metadata[idx - 1].split(' ')[0].replace('.', ''))
+        except Exception as e:
+            n_reviews = 0
+            n_photos = 0
+        user_url = review.find('a')['href']
+
+        item['id_review'] = id_review
+        item['caption'] = review_text
+        item['relative_date'] = relative_date
+        item['retrieval_date'] = datetime.now()
+        # retrieval_date - time(relative_date)
+        item['rating'] = rating
+        item['username'] = username
+        item['n_review_user'] = n_reviews
+        item['n_photo_user'] = n_photos
+        item['url_user'] = user_url
+        return item
+
+    def __parse_place(self, response):
+        place = {}
+        try:
+            place['overall_rating'] = float(response.find('div', class_='gm2-display-2').text.replace(',', '.'))
+        except:
+            place['overall_rating'] = 'NOT FOUND'
+        try:
+            place['n_reviews'] = int(response.find('div', class_='gm2-caption').text.replace('.', '').replace(',','').split(' ')[0])
+        except:
+            place['n_reviews'] = 0
+        return place
+
+    def __expand_reviews(self):
+        links = self.driver.find_elements_by_xpath('//button[@class=\'section-expand-review blue-link\']')
+        for l in links:
+            l.click()
+        time.sleep(2)
+
+    def __scroll(self):
+        scrollable_div = self.driver.find_element_by_css_selector('div.section-layout.section-scrollbox.scrollable-y.scrollable-show')
+        self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', scrollable_div)
+
+    def __get_logger(self):
+        logger = logging.getLogger('GoogleMapsScraper')
+        logger.setLevel(logging.DEBUG)
+        fh = logging.FileHandler('GoogleMapsScraper.log')
+        fh.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+        return logger
+
+    def __get_driver(self, debug=False):
+        options = Options()
+        if not self.debug:
+            options.add_argument("--headless")
+        else:
+            options.add_argument("--window-size=1366,768")
+        options.add_argument("--disable-notifications")
+        options.add_argument("--lang=en-GB")
+        input_driver = webdriver.Chrome(chrome_options=options)
+        return input_driver
+
+    def __filter_string(self, str):
+        strOut = str.replace('\r', ' ').replace('\n', ' ').replace('\t', ' ')
+        return strOut
