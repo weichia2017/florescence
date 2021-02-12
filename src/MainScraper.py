9736617c995281032f6db56a46febac48cd9adda
Chia Wei <43131747+weichia2017@users.noreply.github.com>
2021-02-12 16:18:11 +0800

    Scrape a single store completed
    Next update: Multiple stores

--- src/MainScraper.py
+++ src/MainScraper.py
@@ -1,33 +1,35 @@
-import time
-import datetime
+import time, datetime, logging, traceback
 from random import randrange
 from GoogleReviewScraper import GoogleReviewScraper
 from TripAdvisorScraper import TripAdvisorScraper
 from DatabaseAccess import DataAccess
 
-
 class MainScraper:
-
     def __init__(self, debug_mode=False):
         self.debug_mode = debug_mode
         self.logger = self.__get_logger()
 
-    def ScrapeStore(self, store_id):
+    def scrapeStore(self, store_id):
+        self.logger.info("Starting Scrape for Store ID:" + str(store_id))
         with DataAccess() as dao:
             stores = dao.getStore(store_id, True)
             stores.dropna(inplace=True)
             stores.reset_index(inplace=True)
             for index, row in stores.iterrows():
                 if (row['googlereviews_url'] != ""):
+                    self.logger.info("Executing Google Scraping Function")
                     self.__scrape_google(row, dao)
                 if (row['tripadvisor_url'] != ""):
+                    self.logger.info("Executing TripAdvisor Scraping Function")
                     self.__scrape_tripadvisor(row, dao)
+        self.logger.info("Scrape completed")
 
     def __scrape_google(self, row, dao):
+        number_to_scrape = 1000
         store_id = row['store_id']
         store_name = row['store_name']
         store_url = row['googlereviews_url']
-        with Scraper(debug=debug_mode) as scraper:
+        with GoogleReviewScraper(debug=self.debug_mode) as scraper:
             scraper.setURL(url=store_url, name=store_name)
             noError = scraper.sort_by_date()
             if noError == True:
@@ -45,10 +47,11 @@ class MainScraper:
                         break
 
     def __scrape_tripadvisor(self, row, dao):
+        dateUpdated = datetime.datetime.now().strftime("%Y:%m:%d")
         store_id = row['store_id']
         store_name = row['store_name']
         store_url = row['tripadvisor_url']
-        with TripAdvisorScraper(debug=debug_mode) as scraper:
+        with TripAdvisorScraper(debug=self.debug_mode) as scraper:
             doesNotExistInDB = True
             scraper.setURL(url=store_url, name=store_name)
             self.logger.info("Looking for new review(s) for " + store_name)
