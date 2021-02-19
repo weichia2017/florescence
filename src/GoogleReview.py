983c46d1eb8943f4f267a9f3af9b27d879a70993
Chia Wei <43131747+weichia2017@users.noreply.github.com>
2021-02-19 13:19:48 +0800

    Lint and Cleanup
    Lint - Formatting
    Cleanup imports that isn't being used

--- src/GoogleReview.py
+++ src/GoogleReview.py
@@ -2,12 +2,13 @@ from datetime import datetime
 from datetime import timedelta
 from dateutil.relativedelta import relativedelta
 
+
 class GoogleReview:
     """GoogleReview contains information for individual reviews.
 
     This object is initialized by GoogleReviewScraper by passing in the review string
     from the Google Maps Review webpage.
-    
+
     Attributes:
         review_id: The Unique Code for the review
         username: Username of the reviewer
@@ -20,25 +21,33 @@ class GoogleReview:
         user_url: URL to the profile of the reviewer
         retrieval_date: The date that this review for retrieved.
     """
+
     def __init__(self, review):
-        self.review_id = review.find('button', class_='section-review-action-menu')['data-review-id']
-        self.username = review.find('div', class_='section-review-title').find('span').text
+        self.review_id = review.find(
+            'button', class_='section-review-action-menu')['data-review-id']
+        self.username = review.find(
+            'div', class_='section-review-title').find('span').text
         try:
-            review_text = self.__filter_string(review.find('span', class_='section-review-text').text)
+            review_text = self.__filter_string(review.find(
+                'span', class_='section-review-text').text)
         except Exception as e:
             review_text = None
         self.review_text = review_text
-        self.rating = float(review.find('span', class_='section-review-stars')['aria-label'].split(' ')[1])
-        self.relative_date = review.find('span', class_='section-review-publish-date').text
+        self.rating = float(review.find(
+            'span', class_='section-review-stars')['aria-label'].split(' ')[1])
+        self.relative_date = review.find(
+            'span', class_='section-review-publish-date').text
         try:
-            n_reviews_photos = review.find('div', class_='section-review-subtitle').find_all('span')[1].text
+            n_reviews_photos = review.find(
+                'div', class_='section-review-subtitle').find_all('span')[1].text
             metadata = n_reviews_photos.split('\xe3\x83\xbb')
             if len(metadata) == 3:
                 n_photos = int(metadata[2].split(' ')[0].replace('.', ''))
             else:
                 n_photos = 0
             idx = len(metadata)
-            self.n_review_user = int(metadata[idx - 1].split(' ')[0].replace('.', ''))
+            self.n_review_user = int(
+                metadata[idx - 1].split(' ')[0].replace('.', ''))
             self.n_photo_user = n_photos
         except Exception as e:
             self.n_review_user = 0
