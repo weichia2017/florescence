73557e3adcef75b4105369ff6e190bbf10c0c624
Chia Wei <43131747+weichia2017@users.noreply.github.com>
2021-02-23 13:21:21 +0800

    new routes

--- api/__init__.py
+++ api/__init__.py
@@ -11,7 +11,19 @@ def hello():
 @app.route('/reviews/<int:store_id>')
 def reviewsForStore(store_id):
     dao = DataAccess()
+    df = dao.getStoreReviews(store_id)
+    result = df.to_json(orient="records")
+    parsed = json.loads(result)
+    return json.dumps(parsed, indent=4)
+
+@app.route('/sentiments/<int:store_id>')
+def sentimentsForStore(store_id):
+    dao = DataAccess()
     raw_df = dao.getStoreReviews(store_id)
+    c = Cleaner(raw_df)
+    c.separateEmptyReview()
+    c.remove_translated()
+    df = c.get_df()
     result = raw_df.to_json(orient="records")
     parsed = json.loads(result)
     return json.dumps(parsed, indent=4)
