0f7f5f665ae1f310b05e30010b19ad8329f40f73
Chia Wei <43131747+weichia2017@users.noreply.github.com>
2021-02-26 17:24:28 +0800

    flask_caching installed
    This reduces the server load and improve loading times for subsequent requests

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import jsonify
+from flask_caching import Cache
+
 import sys
 import numpy as np
 import pandas as pd
@@ -7,7 +9,11 @@ sys.path.insert(1, 'florescence/src')
 from DatabaseAccess import DataAccess
 from MasterReview import Master
 
+LIGHT_CACHE = 60
+HEAVY_CACHE = 60*60
+
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 dao = DataAccess()
 
 @app.route('/')
@@ -15,16 +21,19 @@ def hello():
     return 'Hello, World!'
 
 @app.route('/store/')
+@cache.cached(timeout=LIGHT_CACHE)
 def all_Stores():
     df = dao.getStores().reset_index()
     return __parse_df(df), 200
 
 @app.route('/store/<int:store_id>')
+@cache.cached(timeout=LIGHT_CACHE)
 def one_Store(store_id):
     df = dao.getStore(store_id).reset_index()
     return __parse_df(df), 200
 
 @app.route('/sentiment/<int:store_id>')
+@cache.cached(timeout=HEAVY_CACHE)
 def sentiment(store_id):
     try:
         df = Master(dao.getStoreReviews(store_id)).sentiment_scores()
@@ -33,6 +42,7 @@ def sentiment(store_id):
     return __parse_df(df), 200
 
 @app.route('/adj_noun_pairs/<int:store_id>')
+@cache.cached(timeout=HEAVY_CACHE)
 def adj_noun_pairs(store_id):
     try:
         df = Master(dao.getStoreReviews(store_id)).adj_noun_pairs()
