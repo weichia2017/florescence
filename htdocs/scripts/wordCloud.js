058e086a7f34da87505e8e773abdddad198186a1
Baratharam <baratharamm.2018@sis.smu.edu.sg>
2021-03-01 13:19:51 +0800

    WordCloud with noun adj pairs
    1. No longer using CDN for the wordcloudlibrary. Downloaded the library to scripts folder
    2. Wordcloud still hardcoded but working with noun adj pairs.
    3. Word cloud is removed and drawn each time the window is resized. This is to ensure the wordcloud is responsive.
    4. Colors are added at random now
    5. Adj are now also clickable. If review id present print the review id in console.log else else print N.A.
    
    TODO:
    1. Use google fonts so mobile devices will display the wordcloud font as well.
    2. Font starting size shld be modified based on the device size
    3. No.of NOUN inputs shld be less rigid.
    4. Get data dynamically for each page refresh
    5. Show a list of reviews when user selects a particular adj

--- htdocs/scripts/wordCloud.js
+++ htdocs/scripts/wordCloud.js
@@ -1,62 +1,305 @@
-// HardCoded Values
-const myWords = [
+let jsonResponse = `
+    {
+        "data": [{
+                "ADJ": "good",
+                "COUNT": 11,
+                "NOUN": "wine",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "great",
+                "COUNT": 8,
+                "NOUN": "wine",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "expensive",
+                "COUNT": 4,
+                "NOUN": "wine",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "same",
+                "COUNT": 3,
+                "NOUN": "time",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "long",
+                "COUNT": 2,
+                "NOUN": "time",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "last",
+                "COUNT": 2,
+                "NOUN": "time",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "friendly",
+                "COUNT": 5,
+                "NOUN": "staff",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "knowledgeable",
+                "COUNT": 3,
+                "NOUN": "staff",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "expensive",
+                "COUNT": 1,
+                "NOUN": "staff",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "great",
+                "COUNT": 4,
+                "NOUN": "service",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "excellent",
+                "COUNT": 3,
+                "NOUN": "service",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "good",
+                "COUNT": 2,
+                "NOUN": "service",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "good",
+                "COUNT": 7,
+                "NOUN": "selection",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "great",
+                "COUNT": 5,
+                "NOUN": "selection",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "extensive",
+                "COUNT": 2,
+                "NOUN": "selection",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "reasonable",
+                "COUNT": 3,
+                "NOUN": "price",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "good",
+                "COUNT": 2,
+                "NOUN": "price",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "steep",
+                "COUNT": 1,
+                "NOUN": "price",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "great",
+                "COUNT": 7,
+                "NOUN": "place",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "good",
+                "COUNT": 5,
+                "NOUN": "place",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "cozy",
+                "COUNT": 4,
+                "NOUN": "place",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "extensive",
+                "COUNT": 5,
+                "NOUN": "list",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "wide",
+                "COUNT": 1,
+                "NOUN": "list",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "uptodate",
+                "COUNT": 1,
+                "NOUN": "list",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "good",
+                "COUNT": 10,
+                "NOUN": "food",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "great",
+                "COUNT": 5,
+                "NOUN": "food",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "delicious",
+                "COUNT": 2,
+                "NOUN": "food",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "best",
+                "COUNT": 2,
+                "NOUN": "bar",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "great",
+                "COUNT": 2,
+                "NOUN": "bar",
+                "REVIEWID": "1f23f23fff"
+            },
+            {
+                "ADJ": "little",
+                "COUNT": 2,
+                "NOUN": "bar",
+                "REVIEWID": "1f23f23fff"
+            }
+        ],
+        "datetime": "Sun, 28 Feb 2021 16:01:59 GMT"
+    }`;
 
-    {
-    "ADJ": ["good","great","expensive"], 
-    "COUNT": 23, 
-    "NOUN": "wine"
-    },
-    {
-    "ADJ": ["same","long","last"], 
-    "COUNT": 7, 
-    "NOUN": "time"
-    }, 
-    {
-    "ADJ": ["friendly","knowledgeable","expensive"], 
-    "COUNT": 9, 
-    "NOUN": "staff"
-    }, 
-    {
-    "ADJ": ["great","excellent","good"], 
-    "COUNT": 9, 
-    "NOUN": "service"
-    }, 
-    {
-    "ADJ": ["good","great","extensive"], 
-    "COUNT": 14, 
-    "NOUN": "selection"
-    }, 
-    {
-    "ADJ": ["reasonable","good","steep"], 
-    "COUNT": 6, 
-    "NOUN": "price"
-    }, 
-    {
-    "ADJ": ["great","good","cozy"], 
-    "COUNT": 16, 
-    "NOUN": "place"
-    }, 
-    {
-    "ADJ": ["extensive","wide","uptodate"], 
-    "COUNT": 7, 
-    "NOUN": "list"
-    }, 
-    {
-    "ADJ": ["good","great","delicious"], 
-    "COUNT": 17, 
-    "NOUN": "food"
-    }, 
-    {
-    "ADJ": ["best","great","little"], 
-    "COUNT": 6, 
-    "NOUN": "bar"
+
+let preparedData  = [];
+var width         = $(window).width(), height = $(window).height();
+
+function prepareWordCloud(){
+    let response          = JSON.parse(jsonResponse).data;
+    let accumulatedAdj    = [];
+    let totalCountForNoun = 0;
+    for(x in response){
+        let currentNoun   = response[x].NOUN;
+        let count         = response[x].COUNT;
+        let adj           = response[x].ADJ;
+        let reviewid      = 'N.A';
+        if(response[x].REVIEWID != undefined){
+            reviewid      = response[x].REVIEWID;
+        }
+       
+        totalCountForNoun += count;
+        //adj parameter 0,1,2: the adj, the count of adj, the reviewID of adj
+        accumulatedAdj.push([adj,count,reviewid]);
+
+        if( (response[parseInt(x)+1] !== undefined && currentNoun != response[parseInt(x)+1].NOUN) || 
+            response[parseInt(x)+1] === undefined){
+
+            // adj parameter 3: adj sizes
+            let wordCloudSize = Math.log10(totalCountForNoun)*60;
+            let adjOneSize   = wordCloudSize/6 *2.5;
+            let adjTwoSize   = wordCloudSize/6 *2;
+            let adjThreeSize = wordCloudSize/6 *1.5;
+
+            fontFamily = 'Impact';
+            spaceBetweenNounAdj = 3;
+            let nounLength = getTextLength(currentNoun,wordCloudSize,fontFamily) /2;
+
+            // adj parameter 4: How much the adj shld move in the x axis
+            let adjOneX   =  (nounLength + spaceBetweenNounAdj);
+            let adjTwoX   =  (nounLength + spaceBetweenNounAdj);
+            let adjThreeX =  (nounLength + spaceBetweenNounAdj);
+
+            // adj parameter 5: How much the adj shld move in the y axis
+            let adjOneY   = wordCloudSize/6 * 2.7;
+            let adjTwoY   = wordCloudSize/6 * 1.1;
+            let adjThreeY = 2;
+
+            accumulatedAdj[0].push(adjOneSize,adjOneX,adjOneY);
+            accumulatedAdj[1].push(adjTwoSize,adjTwoX,adjTwoY);
+            accumulatedAdj[2].push(adjThreeSize,adjThreeX,adjThreeY);
+
+
+            let newTempNoun = getNewNounDupe(accumulatedAdj,fontFamily,spaceBetweenNounAdj,nounLength,wordCloudSize,currentNoun);
+            console.log(newTempNoun);
+
+            preparedData.push({ noun  :  newTempNoun,
+                                adj   :  accumulatedAdj,
+                                count :  totalCountForNoun,
+                                size  : wordCloudSize
+                            });
+            totalCountForNoun = 0;
+            accumulatedAdj    = [];
+        }
     }
-];
+    console.log(preparedData)
 
-var w = 650
-var h = 450
+    let w = document.getElementById('wordCloudContainer').offsetWidth;
+    let h = 450;
 
-drawWordcLOUD(w,h)
+    drawWordcLOUD(w,h);
+}
+
+function getTextLength(text,size,fontFamily){
+
+    var BrowserText = (function () {
+        var canvas = document.createElement('canvas'),
+            context = canvas.getContext('2d');
+    
+        /**
+         * Measures the rendered width of arbitrary text given the font size and font face
+         * @param {string} text The text to measure
+         * @param {number} fontSize The font size in pixels
+         * @param {string} fontFace The font face ("Arial", "Helvetica", etc.)
+         * @returns {number} The width of the text
+         **/
+        function getWidth(text, fontSize, fontFace) {
+            context.font = fontSize + 'px ' + fontFace;
+            return context.measureText(text).width;
+        }
+    
+        return {
+            getWidth: getWidth
+        };
+    })();
+
+    return BrowserText.getWidth(text, size, fontFamily);
+}
+
+function getNewNounDupe(adjArray,fontFamily,spaceBetweenNounAdj,nounLength,nounSize,nounText){
+    let longestAdj = 0;
+    for (x in adjArray){
+        length = getTextLength(adjArray[x][0],adjArray[x][3],fontFamily);
+
+        if(length > longestAdj){
+            longestAdj = length;
+        }
+    }
+
+    totalLength = longestAdj + spaceBetweenNounAdj + (nounLength*2);
+
+    let newNounText = "."+nounText;
+
+    do{
+        newNounText = "l" + newNounText;
+    }
+    while (getTextLength(newNounText,nounSize,fontFamily) <= totalLength)
+
+    return "ll" + newNounText;
+}
 
 /* Each time the window gets resized, 
 *   1. get the new width and height of the container
@@ -68,24 +311,30 @@ function resize(){
     // console.log(document.getElementById('wordCloudContainer').offsetHeight)
     w = document.getElementById('wordCloudContainer').offsetWidth;
     h = document.getElementById('wordCloudContainer').offsetHeight;
-    removeWordCloud();
-    drawWordcLOUD(w,h);
+
+    if($(window).width() != width || $(window).height() != height){
+        removeWordCloud();
+        drawWordcLOUD(w,h);
+    }
 }
 
 function removeWordCloud(){
-    document.getElementById("wordCloudContainer").innerHTML = ""
-    // wordCloudContainer = d3.select("#wordCloudContainer");
-    // wordCloudContainer.selectAll("*").remove()
+    document.getElementById("wordCloudContainer").innerHTML = "";
 }
 
-function drawWordcLOUD(){
+function randomColor () {
+    var chars = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++)
+        color += chars[Math.floor(Math.random() * 16)];
+    return color;
+}
+
+function drawWordcLOUD(w,h){
     // set the dimensions and margins of the graph
     var margin = {top: 5, right: 5, bottom: 5, left: 5},
     width = w - margin.left - margin.right,
     height = h - margin.top - margin.bottom;
-
-    console.log(width)
-    console.log(height)
 
     // append the svg object to the body of the page
     var svg = d3.select("#wordCloudContainer").append("svg")
@@ -97,41 +346,152 @@ function drawWordcLOUD(){
 
     // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
     // Wordcloud features that are different from one word to the other must be here
-    var layout = d3.layout.cloud()
+    var wordCloudSVG = d3.layout.cloud() 
     .size([width, height])
-    .words(myWords.map(function(d) { return {text: d.NOUN, size:d.COUNT}; }))
-    .padding(10)        //space between words
+    .words(preparedData.map(function(d) { return {text: d.noun, size:d.size, adjArray: d.adj}; }))
+    // .spiral("rectangular")
+    .padding(10)       //space between words
     .rotate(0)         // To rotate -> function() { return ~~(Math.random() * 2) * 90; }
-    .fontSize(function(d) { return  Math.log10(d.size)*60; })  // Originial is just d.size ...; Log Math.log10(d.size)*60; Initiall used Math.abs(d.size - average)/average * 60
-    .on("end", draw);
-    layout.start();
+    .fontSize(function(d) { return d.size})  // Originial is just d.size ...; Log Math.log10(d.size)*60; Initiall used Math.abs(d.size - average)/average * 60
+    .on("word", ({size, x, y, rotate, text, adjArray}) => {
+        // console.log(x)
+        console.log(size)
+        let nounSize = size;
+        let nounX    = x;
+        let nounY    = y;
 
-    // This function takes the output of 'layout' above and draw the words
-    // Wordcloud features that are THE SAME from one word to the other can be here
-    function draw(words) {
-    svg
-        // .append("g")
-        .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-        .selectAll("text")
-        .data(words)
-        .enter().append("text")
-        .style("font-size", function(d) { return d.size; })  
-        .style("fill", color => randomColor())
-        .attr("text-anchor", "middle")
-        .style("font-family", "Impact")
-        .attr("transform", function(d) {
-        return "translate(" + [d.x, d.y]  + ")rotate(" + d.rotate + ")";
-        })
-        .text(function(d) { return d.text; }) 
-    }
+        let adjTextOne   = adjArray[0][0];
+        let adjTextTwo   = adjArray[1][0];
+        let adjTextThree = adjArray[2][0];
+
+        let adjOneSize   = adjArray[0][3];
+        let adjTwoSize   = adjArray[1][3];
+        let adjThreeSize = adjArray[2][3];
+
+        let adjOneX   = x - adjArray[0][4];
+        let adjTwoX   = x - adjArray[1][4];
+        let adjThreeX = x - adjArray[2][4];
+
+        let adjOneY   = y - adjArray[0][5];
+        let adjTwoY   = y - adjArray[1][5];
+        let adjThreeY = y + adjArray[2][5];
+
+        //NOUN
+        svg.append("text")
+          .attr("font-size", nounSize)
+          .attr("text-anchor", "middle")
+          .style("font-family", 'Impact')
+          .attr("transform", `translate(${nounX},${nounY}) rotate(${rotate})`)
+          .text(text.split(".")[1])
+          .classed("click-only-text", true)
+          .style("fill", color => randomColor());
+
+        //ADJ1
+        svg.append("text")
+          .attr("font-size", adjOneSize)
+          .attr("text-anchor", "end")
+          .attr("id",  adjArray[0][2])
+          .style("font-family", 'Impact')
+          .attr("transform", `translate(${adjOneX},${adjOneY}) rotate(${rotate})`)
+          .text(adjTextOne)
+          .classed("click-only-text", true)
+          .style("fill", color => randomColor())
+          .on("mouseover", handleMouseOverAdjOne)
+          .on("mouseout", handleMouseOutAdjOne)
+          .on("click", handleClick);
+        
+          function handleMouseOverAdjOne(d, i) {
+            d3.select(this)
+              .classed("word-hovered", true)
+              .transition(`mouseover-${adjTextOne}`).duration(300).ease(d3.easeLinear)
+                .attr("font-size", adjOneSize + 3);
+          }
+          
+          function handleMouseOutAdjOne(d, i) {
+            d3.select(this)
+              .classed("word-hovered", false)
+              .interrupt(`mouseover-${adjTextOne}`)
+                .attr("font-size", adjOneSize);
+          }
+          
+          function handleClick(d, i) {
+            var e = d3.select(this);
+            console.log(e._groups[0][0].id)
+            e.classed("word-selected", !e.classed("word-selected"));
+          }
+
+        //ADJ2
+        svg.append("text")
+          .attr("font-size", adjTwoSize)
+          .attr("text-anchor", "end")
+          .attr("id",  adjArray[1][2])
+          .style("font-family", 'Impact')
+          .attr("transform", `translate(${adjTwoX},${adjTwoY}) rotate(${rotate})`)
+          .text(adjTextTwo)
+          .classed("click-only-text", true)
+          .style("fill", color => randomColor())
+          .on("mouseover", handleMouseOverAdjTwo)
+          .on("mouseout", handleMouseOutAdjTwo)
+          .on("click", handleClick);
+                
+          function handleMouseOverAdjTwo(d, i) {
+            d3.select(this)
+              .classed("word-hovered", true)
+              .transition(`mouseover-${adjTextTwo}`).duration(300).ease(d3.easeLinear)
+                .attr("font-size", adjTwoSize + 3);
+          }
+          
+          function handleMouseOutAdjTwo(d, i) {
+            d3.select(this)
+              .classed("word-hovered", false)
+              .interrupt(`mouseover-${adjTextTwo}`)
+                .attr("font-size", adjTwoSize);
+          }
+          
+          function handleClick(d, i) {
+            var e = d3.select(this);
+            console.log(e._groups[0][0].id)
+            e.classed("word-selected", !e.classed("word-selected"));
+          }
+
+
+        //ADJ3
+        svg.append("text")
+          .attr("font-size", adjThreeSize)
+          .attr("text-anchor", "end")
+          .attr("id",  adjArray[2][2])
+          .style("font-family", 'Impact')
+          .attr("transform", `translate(${adjThreeX},${adjThreeY}) rotate(${rotate})`)
+          .text(adjTextThree)
+          .classed("click-only-text", true)
+          .style("fill", color => randomColor())
+          .on("mouseover", handleMouseOverAdjThree)
+          .on("mouseout", handleMouseOutAdjThree)
+          .on("click", handleClick);
+                
+          function handleMouseOverAdjThree(d, i) {
+            d3.select(this)
+              .classed("word-hovered", true)
+              .transition(`mouseover-${adjTextThree}`).duration(300).ease(d3.easeLinear)
+                .attr("font-size", adjThreeSize + 3);
+          }
+          
+          function handleMouseOutAdjThree(d, i) {
+            d3.select(this)
+              .classed("word-hovered", false)
+              .interrupt(`mouseover-${adjTextThree}`)
+                .attr("font-size", adjThreeSize);
+          }
+          
+          function handleClick(d, i) {
+            var e = d3.select(this);
+            console.log(e._groups[0][0].id)
+            e.classed("word-selected", !e.classed("word-selected"));
+          }
+      });
+
+    wordCloudSVG.start();
 }
 
-function randomColor () {
-    var chars = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++)
-        color += chars[Math.floor(Math.random() * 16)];
-    return color;
-};
-
-$(window).resize(resize)
+$(document).ready(prepareWordCloud);
+$(window).resize(resize);
