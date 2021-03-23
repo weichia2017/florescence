faa079ba27b401bfa2feac9ddc08dacf0f28aa54
Chia Wei <43131747+weichia2017@users.noreply.github.com>
2021-03-23 22:47:04 +0800

    Update Functions to new backend structure

--- api/__init__.py
+++ api/__init__.py
@@ -1,20 +1,19 @@
+import sys
+sys.path.insert(1, 'florescence/src')
+from MasterReview import Master
+from DatabaseAccess import DataAccess
 from datetime import datetime, timezone
 import pandas as pd
 import numpy as np
-import os, sys
-from flask import request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_caching import Cache
-from flask import jsonify
-from flask import Flask
-sys.path.insert(1, 'florescence/src')
-from DatabaseAccess import DataAccess
-from MasterReview import Master
 
 app = Flask(__name__)
 CORS(app)
 cache = Cache(app, config={'CACHE_TYPE': 'simple',
                            'CACHE_DEFAULT_TIMEOUT': 300})
+cache.init_app(app)
 LESS_UPDATES = 60*60*24*7
 MORE_UPDATES = 60*60*24
 
@@ -64,28 +63,10 @@ def reviews(store_id):
             return __response_error(e)
     return __response_ok(df)
 
+
 @app.route('/adj_noun_pairs/<int:store_id>')
 @cache.cached(timeout=LESS_UPDATES)
 def adj_noun_pairs(store_id):
-    with DataAccess() as dao:
-        if store_id not in dao.getStores().index.to_list():
-            return __response_invalid("The store identifier was not found")
-        try:
-            df = Master(dao.getStoreReviews(store_id)).adj_noun_pairs()
-        except Exception as e:
-            return __response_error(e)
-    return __response_ok(df)
-
-@app.route('/adj_noun_pairs/', methods=["POST"])
-def get_adj_noun_pair():
-    request_data = request.get_json()
-    df = pd.json_normalize(request_data, record_path=['data'])
-    df = Master(df).adj_noun_pairs()
-    return __response_ok(df)
-
-@app.route('/test/adj_noun_pairs/<int:store_id>')
-@cache.cached(timeout=LESS_UPDATES)
-def test_adj_noun_pairs(store_id):
     df = None
     with DataAccess() as dao:
         if store_id not in dao.getStores().index.to_list():
@@ -94,17 +75,37 @@ def test_adj_noun_pairs(store_id):
             all_pairs = dao.getAdjNounPairs(store_id)
             num_of_noun = 10
             num_of_adj_each = 3
-            top_noun = all_pairs.groupby(['noun']).size().reset_index(name='count').sort_values(['count'], ascending=False).head(num_of_noun)
-            filtered_nouns = all_pairs[all_pairs.noun.isin(top_noun.noun.to_list())]
-            noun_top_adj_ranking = filtered_nouns.groupby(['noun','adj']).size().reset_index(name='count').sort_values(['count'], ascending=False)
-            filtered_adj = filtered_nouns[filtered_nouns.adj.isin(noun_top_adj_ranking.adj.to_list())]
-            pairs = filtered_adj.groupby(['noun','adj'])['review_id'].apply(','.join).reset_index()
+            top_noun = all_pairs.groupby(['noun']).size().reset_index(
+                name='count').sort_values(['count'], ascending=False).head(num_of_noun)
+            filtered_nouns = all_pairs[all_pairs.noun.isin(
+                top_noun.noun.to_list())]
+            noun_top_adj_ranking = filtered_nouns.groupby(['noun', 'adj']).size(
+            ).reset_index(name='count').sort_values(['count'], ascending=False)
+            filtered_adj = filtered_nouns[filtered_nouns.adj.isin(
+                noun_top_adj_ranking.adj.to_list())]
+            pairs = filtered_adj.groupby(['noun', 'adj'])[
+                'review_id'].apply(','.join).reset_index()
             pairs['review_id'] = pairs.review_id.apply(lambda x: x.split(","))
             pairs['count'] = pairs.review_id.apply(lambda x: len(x))
-            df = pairs.groupby(['noun']).apply(lambda x: x.nlargest(num_of_adj_each,['count'], keep='first')).reset_index(drop=True).sort_values(['noun','count'], ascending=False).reset_index(drop=True)
+            df = pairs.groupby(['noun']).apply(lambda x: x.nlargest(num_of_adj_each, ['count'], keep='first')).reset_index(
+                drop=True).sort_values(['noun', 'count'], ascending=False).reset_index(drop=True)
         except Exception as e:
             return __response_error(e)
     return __response_ok(df)
+
+
+@app.route('/adj_noun_pairs/', methods=["POST"])
+def get_adj_noun_pair():
+    request_data = request.get_json()
+    df = pd.json_normalize(request_data, record_path=['data'])
+    df = Master(df).adj_noun_pairs()
+    return __response_ok(df)
+
+#TODO: remove test function
+@app.route('/test/adj_noun_pairs/<int:store_id>')
+@cache.cached(timeout=LESS_UPDATES)
+def test_adj_noun_pairs(store_id):
+    return adj_noun_pairs(store_id)
 
 def __response_invalid(msg):
     return jsonify(error=msg), 500
