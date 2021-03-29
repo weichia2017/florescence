129545dc5dbe14ddc0175de4bb1c4fbedd4896c2
Chia Wei <43131747+weichia2017@users.noreply.github.com>
2021-03-29 19:56:13 +0800

    Remove cache
    No need for any more memory consumption by caching as all data already pre-rendered.

--- api/__init__.py
+++ api/__init__.py
@@ -3,12 +3,10 @@ sys.path.insert(1, 'florescence/src')
 from datetime import datetime, timezone
 from flask import Flask
 from flask_cors import CORS
-#from flask_caching import Cache
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    #cache = Cache(app, config={'CACHE_TYPE': 'simple', 'CACHE_DEFAULT_TIMEOUT': 3600})
 
     from . import stores, reviews, adj_noun_pairs
     app.register_blueprint(stores.bp)
