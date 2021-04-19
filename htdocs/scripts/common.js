3861b35fc980441003d53ab921ff664f14b6926c
Baratharam <baratharamm.2018@sis.smu.edu.sg>
2021-04-19 19:26:30 +0800

    Overall Dashboard - Common.js and Navbar trial
    1. For both URA and BIZ Dashboards, now they will run makeRequest from common.js. I will add more of the common stuff in here so that when one changes it will affect both URA and BIZ Dashboards.
    
    2. Navbar trial on URADashboard.
      2.1 Dif pop up on mobile and dekstop based on @media types
      2.2 Cursive font for Our Groups Brand Name
    
    3. Background color changed on URADashboard

---
+++ htdocs/scripts/common.js
@@ -0,0 +1,62 @@
+function makeRequest(url,method,values) {
+    return new Promise(function (resolve, reject) {
+
+      let request = new XMLHttpRequest();
+
+      request.open(method, url);
+      request.timeout = 20000;
+      request.onload = function () {
+          if (this.status >= 200 && this.status < 300) {
+              resolve(request.response);
+          } else {
+              reject({
+                  status: this.status,
+                  statusText: request.statusText
+              });
+          }
+      };
+      request.onerror = function () {
+          reject({
+              status: this.status,
+              statusText: request.statusText
+          });
+      };
+      // request.setRequestHeader('Authorization', 'Bearer ' + token)
+      request.setRequestHeader("Content-type", "application/JSON");
+      request.setRequestHeader('Access-Control-Allow-Origin', '*');
+      // request.withCredentials = false;
+      request.send(values);
+    });
+}
+
+function makeRequestxwwwFormURLEncode(url,method,values) {
+    return new Promise(function (resolve, reject) {
+
+    let request = new XMLHttpRequest();
+
+    request.open(method, url,true);
+    request.timeout = 5000;
+    request.onload = function () {
+        if (this.status >= 200 && this.status <= 500) {
+            resolve(request.response);
+        } else {
+            reject({
+                status: this.status,
+                statusText: request.statusText
+            });
+        }
+    };
+    request.onerror = function () {
+        reject({
+            status: this.status,
+            statusText: request.statusText
+        });
+    };
+
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.send(values);
+
+    });
+}
+
+var hostname = "http://35.175.55.18:5000";
